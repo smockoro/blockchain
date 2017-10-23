9143ba4955ac3d2acde4e10e1f06bfe60d5bd01e
Takahiro Oshima <tarotora51@gmail.com>
2017-10-23 09:58:56 +0900

    完成

--- blockchain.py
+++ blockchain.py
@@ -8,19 +8,41 @@
 import requests
 import hashlib
 import json
+from textwrap import dedent
 from time import time
+from uuid import uuid4
+from urllib.parse import urlparse
+import requests
+from flask import Flask, jsonify, request
 
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
         self.new_block(previous_hash=1, proof=100)
+        self.nodes = set()
 
-    def new_block(self):
-        # 新しいブロックを作りチェーンに加える
-        pass
+    def new_block(self, proof, previous_hash=None):
+        """
+        ブロックチェーンに新しいブロックを作成する
+        :param proof: <int> フルーフオブワークアルゴリズムにより得られるプルーフ
+        :param previous_hash: <str> 前のブロックのハッシュ
+        :return: <dict> 新しいブロック
+        """
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
 
-    def new_transaction(self):
+        self.current_transactions = []
+        self.chain.append(block)
+        return block
+ 
+
+    def new_transaction(self, sender, recipient, amount):
         """
         次に採掘されるブロックに加える新しいトランザクションを作る
         :param sender: <str> 送信者のアドレス
@@ -37,13 +59,193 @@ class Blockchain(object):
 
     @staticmethod
     def hash(block):
-        pass
+        """
+        ブロックのSHA-256ハッシュを作る
+        :param block: <dict> ブロック
+        :return: <str>
+        """
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
 
     @property
     def last_block(self):
-        pass
+        return self.chain[-1]
 
-    
+    def proof_of_work(self, last_proof):
+        """
+        単純なプルーフオブワークアルゴリズム
+         - hash(p*p')の最初の4つが0となるようなp'を探す
+         - pは前のプルーフ、p'は新しいプルーフ
+        :param last_proof: <int>
+        :return: <int>
+        """
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        """
+        プルーフが正しいかを確認する: hash(last_proof, proof)の最初の4桁が0かの確認
+        :param last_proof: <int> 前のプルーフ
+        :param proof: <int> 現在のプルーフ
+        :return: <bool> 正しければtrue
+        """
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+
+        return guess_hash[:4] == "0000"
+
+    def register_node(self, address):
+        """
+        ノードリストに新しいノードを加える
+        :param address: <str> ノードのアドレス(Ex. http://192.168.0.5:5000)
+        :return: None
+        """
+        parsed_url = parse(address)
+        self.nodes.add(parsed_url.netloc)
+
+    def valid_chain(self, chain):
+        """
+        ブロックチェーンが正しいかを確認する
+        :param chain: <list> ブロックチェーン
+        :return: <bool> Trueであれば正しい
+        """
+        last_block = chain[0]
+        current_index = 1
+        while current_index < len(chain):
+            block = chain[current_index]
+            print(f'{last_block}')
+            print(f'{block}')
+            print("\n---------------\n")
+
+            if block['previous_hash'] != self.hash(last_block):
+                return False
+
+            if not self.valid_proof(last_block['proof'], block['proof']):
+                return False
+
+            last_block = block
+            current_index += 1
+
+        return True
+
+    def resolve_conflicts(self):
+        """
+        コンセンサスアルゴリズム。ネットワーク上の最も長いチェーンで
+        自らのチェーンを置き換えることでコンフリクトの解消をする。
+        :return: <bool> 自らのチェーンが置き換えられるとTrue
+        """
+        neighbors = self.nodes
+        new_chain = None
+
+        max_length = len(self.chain)
+
+        for nodein neighbors:
+            response = requests.get(f'http://{node}/chain')
+
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+                if length > max_length and self.valid_chain(chain):
+                    max_length = length
+                    new_chain = chain
+
+        if new_chain:
+            self.chain = new_chain
+            return True
+
+        return False
 
 
-if __name__ == "__main__":
+app = Flask(__name__)
+node_identifire = str(uuid4()).replace('-','')
+
+blockchain = Blockchain()
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    values = request.get_json()
+
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+
+    response = {'message': f'トランザクションはブロック{index}に追加されました'}
+    return jsonify(response), 201
+
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+
+    blockchain.new_transaction(
+        sender = "0",
+        recipient = node_identifire,
+        amount = 1,
+    )
+
+    block = blockchain.new_block(proof)
+
+    response = {
+        'message': '新しいブロックを生成しました',
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+    }
+    return jsonify(response), 200
+
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jsonify(response), 200
+
+
+@app.route('/nodes/register', methods=['POST'])
+def register_node():
+    values = request.get_json()
+    nodes = values.get('nodes')
+    if nodes is None:
+        return "Error: 有効ではないノードリストです", 400
+
+    for node in nodes:
+        blockchain.register_node(node)
+
+        response = {
+            'message': '新しいノードが追加されました',
+            'total_nodes': list(blockchain.nodes),
+        }
+        return jsonify(response), 201
+
+
+@app.route('/nodes/resolve', methods=['GET'])
+def consensus():
+    replaced = blockchain.resolve_conflicts()
+
+    if replaced:
+        response = {
+            'message': 'チェーンが置き換えられました',
+            'new_chain': blockchain.chain
+        }
+    else:
+        response = {
+            'message': 'チェーンが確認されました',
+            'new_chain': blockchain.chain
+        }
+
+    return jsonify(response)m 200
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
